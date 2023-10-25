736c9120b54b97bdd3bdee543112260ff223ff63
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:23:19 +0200

    New release 5.0

--- _define.php
+++ _define.php
@@ -12,7 +12,7 @@ $this->registerModule(
     'Café Crème',
     'Structure Aorakit-1B de Kozlika',
     'Franck Paul',
-    '4.0',
+    '5.0',
     [
         'requires' => [['core', '2.28']],
         'type'     => 'theme',
