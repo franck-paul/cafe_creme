ca5b5460b9d2a664b87861910fd250911003b487
franck <carnet.franck.paul@gmail.com>
2025-09-07 14:17:21 +0200

    DC 2.36 compliance

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
     '7.1',
     [
         'date'     => '2025-06-30T10:00:03+0200',
-        'requires' => [['core', '2.35']],
+        'requires' => [['core', '2.36']],
         'type'     => 'theme',
         'overload' => true,
 
